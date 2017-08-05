a6509596621337773fa7c051904f864963120732
Katie Grubbs <katie.grubbs@gmail.com>
2017-08-05 17:51:46 -0400

    Add project files.

---
+++ Pre-SemesterAssignment_VMS/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Pre_SemesterAssignment_VMS
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Pre-SemesterAssignment_VMS/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Pre_SemesterAssignment_VMS.MvcApplication" Language="C#" %>
